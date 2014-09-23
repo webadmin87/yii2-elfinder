9f812623fc897385050f2fd333de78400ac94212
Anton office <webadmin87@gmail.com>
2014-09-23 15:01:00 +0400

    fix bugs

--- Controller.php
+++ Controller.php
@@ -9,10 +9,12 @@
 namespace mihaildev\elfinder;
 
 use Yii;
+use yii\filters\AccessControl;
 use yii\helpers\Json;
+use yii\helpers\Url;
 use yii\web\Controller as BaseController;
 use yii\web\JsExpression;
-use yii\web\NotFoundHttpException;
+
 
 
 /**
@@ -23,97 +25,103 @@ use yii\web\NotFoundHttpException;
 
 
 class Controller extends BaseController{
-    public $roots = [];
-    public $access = '*';
-    public $disabledCommands = ['netmount'];
+	public $roots = [];
+	public $access = ['@'];
+	public $disabledCommands = ['netmount'];
 
-    public function isAvailable(){
-        if($this->access == '*' || Yii::$app->user->can($this->access)){
-            if(!empty($this->options['roots']))
-                return true;
-        }
-
-        return false;
-    }
-
-    private $_options;
-
-    public function getOptions()
-    {
-        if($this->_options !== null)
-            return $this->_options;
-
-        $this->_options['roots'] = [];
-
-        foreach($this->roots as $root){
-            if(is_string($root))
-                $root = ['path' => $root];
-
-            if(!isset($root['class']))
-                $root['class'] = 'mihaildev\elfinder\LocalPath';
-
-            $root = Yii::createObject($root);
-
-            /** @var \mihaildev\elfinder\LocalPath $root*/
-
-            if($root->isAvailable())
-                $this->_options['roots'][] = $root->getRoot();
-        }
-
-        return $this->_options;
-    }
-
-    public function actionConnect(){
-        if(!$this->isAvailable())
-            throw new NotFoundHttpException;
-        return $this->renderFile(__DIR__."/views/connect.php", ['options'=>$this->getOptions()]);
-    }
-
-    public function actionManager(){
-        if(!$this->isAvailable())
-            throw new NotFoundHttpException;
-
-        $options = [
-            'url'=> $this->createUrl('connect'),
-            'customData' => [
-                Yii::$app->request->csrfParam => Yii::$app->request->csrfToken
-            ],
-            'resizable' => false
-        ];
-
-        if(isset($_GET['CKEditor'])){
-            $options['getFileCallback'] = new JsExpression('function(file){ '.
-            'window.opener.CKEDITOR.tools.callFunction('.Json::encode($_GET['CKEditorFuncNum']).', file.url); '.
-            'window.close(); }');
-
-            $options['lang'] = $_GET['langCode'];
-        }
-
-        if(isset($_GET['filter'])){
-            if(is_array($_GET['filter']))
-                $options['onlyMimes'] = $_GET['filter'];
-            else
-                $options['onlyMimes'] = [$_GET['filter']];
-        }
-
-        if(isset($_GET['lang']))
-            $options['lang'] = $_GET['lang'];
-
-        if(isset($_GET['callback'])){
-            $options['getFileCallback'] = new JsExpression('function(file){ '.
-                'if (window!=window.top) {var parent = window.parent;}else{var parent = window.opener;}'.
-                'if(parent.ElFinderFileCallback.callFunction('.Json::encode($_GET['callback']).', file))'.
-                'window.close(); }');
-        }
-
-        if(!isset($options['lang']))
-            $options['lang'] = Yii::$app->language;
-
-        if(!empty($this->disabledCommands))
-            $options['commands'] = new JsExpression('ElFinderGetCommands('.Json::encode($this->disabledCommands).')');
+	public function behaviors()
+	{
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+				'rules' => [
+					[
+						'allow' => true,
+						'roles' => $this->access,
+					],
+				],
+			],
+		];
+	}
 
 
-        return $this->renderFile(__DIR__."/views/manager.php", ['options'=>$options]);
-    }
+	private $_options;
 
-} 
+	public function getOptions()
+	{
+		if($this->_options !== null)
+			return $this->_options;
+
+		$this->_options['roots'] = [];
+
+		foreach($this->roots as $root){
+			if(is_string($root))
+				$root = ['path' => $root];
+
+			if(!isset($root['class']))
+				$root['class'] = 'mihaildev\elfinder\LocalPath';
+
+			$root = Yii::createObject($root);
+
+			/** @var \mihaildev\elfinder\LocalPath $root*/
+
+			if($root->isAvailable())
+				$this->_options['roots'][] = $root->getRoot();
+		}
+
+		return $this->_options;
+	}
+
+	public function actionConnect(){
+		return $this->renderFile(__DIR__."/views/connect.php", ['options'=>$this->getOptions()]);
+	}
+
+	public function actionManager(){
+
+		$options = [
+			'url'=> Url::toRoute('connect'),
+			'customData' => [
+				Yii::$app->request->csrfParam => Yii::$app->request->csrfToken
+			],
+			'resizable' => false
+		];
+
+		if(isset($_GET['CKEditor'])){
+			$options['getFileCallback'] = new JsExpression('function(file){ '.
+				'window.opener.CKEDITOR.tools.callFunction('.Json::encode($_GET['CKEditorFuncNum']).', file.url); '.
+				'window.close(); }');
+
+			$options['lang'] = $_GET['langCode'];
+		}
+
+		if(isset($_GET['filter'])){
+			if(is_array($_GET['filter']))
+				$options['onlyMimes'] = $_GET['filter'];
+			else
+				$options['onlyMimes'] = [$_GET['filter']];
+		}
+
+		if(isset($_GET['lang']))
+			$options['lang'] = $_GET['lang'];
+
+		if(isset($_GET['callback'])){
+			if(isset($_GET['multiple']))
+				$options['commandsOptions']['getfile']['multiple'] = true;
+
+			$options['getFileCallback'] = new JsExpression('function(file){ '.
+				'if (window!=window.top) {var parent = window.parent;}else{var parent = window.opener;}'.
+				'if(parent.ElFinderFileCallback.callFunction('.Json::encode($_GET['callback']).', file))'.
+				'window.close(); }');
+		}
+
+		if(!isset($options['lang']))
+			$options['lang'] = Yii::$app->language;
+
+		if(!empty($this->disabledCommands))
+			$options['commands'] = new JsExpression('ElFinderGetCommands('.Json::encode($this->disabledCommands).')');
+
+
+		return $this->renderFile(__DIR__."/views/manager.php", ['options'=>$options]);
+	}
+
+}
